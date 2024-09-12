bf9dc842a5d1e79d2f1bb0217e9029372aed4942
isha asare <132130662+ishaasare@users.noreply.github.com>
2024-09-12 22:07:57 +0530

    Create Pizza.java

---
+++ Pizza.java
@@ -0,0 +1,17 @@
+public class Pizza {
+    private String name;
+    private int price;
+
+    public Pizza(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
